de2ed5fac66e0c775e636d532d2deaf384a5c8b4
Rasel <mdraselkibria11@gmail.com>
2023-10-02 14:55:52 +0600

    process invoice function work well

--- src/route/api.js
+++ src/route/api.js
@@ -23,13 +23,29 @@ router.get("/ListReview", ProductController.ListReview);
 router.get("/ProductDetails", ProductController.ProductDetails);
 router.get("/ListByRemark/:remark", ProductController.ListByRemark);
 
-router.get("/WishList",AuthVerification, ProductController.WishList);
-router.post("/CreateWishList",AuthVerification, ProductController.CreateWishList);
-router.post("/RemoveWishList",AuthVerification, ProductController.RemoveWishList);
+router.get("/WishList", AuthVerification, ProductController.WishList);
+router.post(
+  "/CreateWishList",
+  AuthVerification,
+  ProductController.CreateWishList
+);
+router.post(
+  "/RemoveWishList",
+  AuthVerification,
+  ProductController.RemoveWishList
+);
 
-router.get("/CartList",AuthVerification, ProductController.CartList);
-router.post("/CreateCartList",AuthVerification, ProductController.CreateCartList);
-router.post("/RemoveCartList",AuthVerification, ProductController.RemoveCartList);
+router.get("/CartList", AuthVerification, ProductController.CartList);
+router.post(
+  "/CreateCartList",
+  AuthVerification,
+  ProductController.CreateCartList
+);
+router.post(
+  "/RemoveCartList",
+  AuthVerification,
+  ProductController.RemoveCartList
+);
 
 // // User
 router.post("/UserLogin/:email", UserController.UserLogin);
@@ -37,14 +53,21 @@ router.post("/VerifyLogin/:email/:otp", UserController.VerifyLogin);
 // router.get("/UserLogout", UserController.UserLogout);
 
 // // Profile
-router.post("/CreateProfile",AuthVerification, ProfileController.CreateProfile);
+router.post(
+  "/CreateProfile",
+  AuthVerification,
+  ProfileController.CreateProfile
+);
 router.get("/ReadProfile", AuthVerification, ProfileController.ReadProfile);
 router.get("/UpdateProfile", AuthVerification, ProfileController.UpdateProfile);
 
 // // Invoice
 router.get("/InvoiceCreate", AuthVerification, InvoiceController.InvoiceCreate);
 router.get("/InvoiceList", AuthVerification, InvoiceController.InvoiceList);
-router.get("/InvoiceProductList", AuthVerification,InvoiceController.InvoiceProductList
+router.get(
+  "/InvoiceProductList",
+  AuthVerification,
+  InvoiceController.InvoiceProductList
 );
 router.get("/PaymentSuccess", InvoiceController.PaymentSuccess);
 router.get("/PaymentCancel", InvoiceController.PaymentCancel);
--- src/services/InvoiceService.js
+++ src/services/InvoiceService.js
@@ -1,88 +1,95 @@
-const CartModel = require("../models/CartModel")
-const ProfileModel = require("../models/ProfileModel")
-const InvoiceModel = require("../models/InvoiceModel")
-const PaymentSettingModel = require("../models/PaymentSettingsModel")
-const axios = require("axios")
-const FormData = require('form-data');
-const mongoose = require("mongoose")
-const ObjectId = mongoose.Types.ObjectId
-const processInvoice =async (req) => {
-    try{
-        let user_id = new ObjectId(req.headers.id)
-        let cus_email=req.headers.email;
-        let data = await CartModel.aggregate([
-            {$match:{userID:user_id}},
-            {$group:{_id:0,sum:{$sum:"$price"}}}
-        ])
-        let payable = data[0].sum
-        let tran_id=Math.floor(100000000 + Math.random() * 900000000);
-        let val_id = 0;
-        let delivery_status = "Pending";
-        let payment_status = "Pending";
-        //get user profile info
-        let Profile = await ProfileModel.aggregate([
-            {$match:{userID:user_id}}
-        ])
-        // Customer Shipping Details
-        let cus_details=`Name: ${Profile[0].cus_name},Email ${Profile[0].cus_email}, Address: ${Profile[0].cus_add}, Phone: ${Profile[0].cus_phone}`
-        let ship_details=`Name: ${Profile[0].ship_name}, City: ${Profile[0].ship_city}, Address: ${Profile[0].ship_add}, Phone: ${Profile[0].ship_phone}`
+const CartModel = require("../models/CartModel");
+const ProfileModel = require("../models/ProfileModel");
+const InvoiceModel = require("../models/InvoiceModel");
+const PaymentSettingsModel = require("../models/PaymentModel");
+const axios = require("axios");
+const FormData = require("form-data");
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
-        // Pending Payment Invoice Create
-        await InvoiceModel.create({
-            userID:user_id,
-            payable:payable,
-            cus_details:cus_details,
-            ship_details:ship_details,
-            tran_id:tran_id,
-            delivery_status:delivery_status,
-            payment_status:payment_status,
-            val_id:val_id
-        })
+const processInvoice = async (req) => {
+  try {
+    let user_id = new ObjectId(req.headers.id);
+    let cus_email = req.headers.email;
+    let data = await CartModel.aggregate([
+      { $match: { userID: user_id } },
+      { $group: { _id: 0, sum: { $sum: { $toDecimal: "$price" } } } },
+    ]);
+    console.log(data);
+    let payable = data[0].sum;
+    let tran_id = Math.floor(100000000 + Math.random() * 900000000);
+    let val_id = 0;
+    let delivery_status = "Pending";
+    let payment_status = "Pending";
 
-        //payment settings
-        let paymentSettings = await PaymentSettingModel.find()
+    //get user profile info
+    let Profile = await ProfileModel.aggregate([
+      { $match: { userID: user_id } },
+    ]);
+    // Customer Shipping Details
+    let cus_details = `Name: ${Profile[0].cus_name},Email ${cus_email}, Address: ${Profile[0].cus_add}, Phone: ${Profile[0].cus_phone}`;
+    let ship_details = `Name: ${Profile[0].ship_name}, City: ${Profile[0].ship_city}, Address: ${Profile[0].ship_add}, Phone: ${Profile[0].ship_phone}`;
 
-        const form = new FormData()
-        form.append('store_id', 'teamr64c9e84055219');
-        form.append('store_passwd', 'teamr64c9e84055219@ssl');
-        form.append('total_amount',payable.toString());
-        form.append('currency', 'BDT');
-        form.append('tran_id', tran_id);
-        form.append('success_url', 'www.ostad.com');
-        form.append('fail_url', 'www.ostad.com');
-        form.append('cancel_url', 'www.ostad.com');
-        form.append('ipn_url', 'www.ostad.com');
+    // Pending Payment Invoice Create
+    await InvoiceModel.create({
+      userID: user_id,
+      payable: payable,
+      cus_details: cus_details,
+      ship_details: ship_details,
+      tran_id: tran_id,
+      delivery_status: delivery_status,
+      payment_status: payment_status,
+      val_id: val_id,
+    });
 
-        form.append('cus_name', Profile[0].cus_name);
-        form.append('cus_email',cus_email);
-        form.append('cus_add1', Profile[0].cus_add);
-        form.append('cus_add2', Profile[0].cus_add);
-        form.append('cus_city', Profile[0].cus_city);
-        form.append('cus_state', Profile[0].cus_state);
-        form.append('cus_postcode', Profile[0].cus_postcode);
-        form.append('cus_country', Profile[0].cus_country);
-        form.append('cus_phone', Profile[0].cus_phone);
-        form.append('cus_fax', Profile[0].cus_phone);
+    //payment settings
+    let paymentSetting = await PaymentSettingsModel.find();
+    console.log(paymentSetting);
 
-        form.append('shipping_method', 'YES');
-        form.append('ship_name', Profile[0].ship_name);
-        form.append('ship_add1', Profile[0].ship_add);
-        form.append('ship_add2', Profile[0].ship_add);
-        form.append('ship_city', Profile[0].ship_city);
-        form.append('ship_state', Profile[0].ship_state);
-        form.append('ship_country', Profile[0].ship_country);
-        form.append('ship_postcode', Profile[0].ship_postcode);
-        form.append('product_name', 'product_name');
-        form.append('product_category', 'category');
-        form.append('product_profile', 'profile');
-        form.append('product_amount', '3');
+    const form = new FormData();
+    form.append("store_id", paymentSetting[0]["store_id"]);
+    form.append("store_passwd", paymentSetting[0]["store_passwd"]);
+    form.append("total_amount", payable.toString());
+    form.append("currency", paymentSetting[0]["currency"]);
+    form.append("tran_id", tran_id);
+    form.append(
+      "success_url",
+      `${paymentSetting[0]["success_url"]}/${tran_id}`
+    );
+    form.append("fail_url", `${paymentSetting[0]["fail_url"]}/${tran_id}`);
+    form.append("cancel_url", `${paymentSetting[0]["cancel_url"]}/${tran_id}`);
+    form.append("ipn_url", `${paymentSetting[0]["ipn_url"]}/${tran_id}`);
 
-        let SSLRes=await axios.post("https://sandbox.sslcommerz.com/gwprocess/v4/api.php",form)
+    form.append("cus_name", Profile[0].cus_name);
+    form.append("cus_email", cus_email);
+    form.append("cus_add1", Profile[0].cus_add);
+    form.append("cus_add2", Profile[0].cus_add);
+    form.append("cus_city", Profile[0].cus_city);
+    form.append("cus_state", Profile[0].cus_state);
+    form.append("cus_postcode", Profile[0].cus_postcode);
+    form.append("cus_country", Profile[0].cus_country);
+    form.append("cus_phone", Profile[0].cus_phone);
+    form.append("cus_fax", Profile[0].cus_phone);
 
-        return {status:"success", message:SSLRes.data}
-    }  catch (e) {
-        return {status:false,message:"Something went wrong",error:e}
-    }
-}
+    form.append("shipping_method", "YES");
+    form.append("ship_name", Profile[0].ship_name);
+    form.append("ship_add1", Profile[0].ship_add);
+    form.append("ship_add2", Profile[0].ship_add);
+    form.append("ship_city", Profile[0].ship_city);
+    form.append("ship_state", Profile[0].ship_state);
+    form.append("ship_country", Profile[0].ship_country);
+    form.append("ship_postcode", Profile[0].ship_postcode);
+    form.append("product_name", "product_name");
+    form.append("product_category", "category");
+    form.append("product_profile", "profile");
+    form.append("product_amount", "3");
 
-module.exports = processInvoice
+    let SSLRes = await axios.post(paymentSetting[0]["init_url"], form);
+
+    return { status: "success", message: SSLRes.data };
+  } catch (e) {
+    return { status: false, message: "Something went wrong", error: e };
+  }
+};
+
+module.exports = processInvoice;
